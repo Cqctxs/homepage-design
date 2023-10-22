292d0a657a946fca5c774fb49a552146ff727efe
Cqctxs <seanzhao2007@gmail.com>
2023-10-22 16:30:28 -0400

    Added images

---
+++ home/src/assets/images/index.js
@@ -0,0 +1,42 @@
+import background from './background.png';
+import black_shadow from './black_shadow.png';
+import fog_1 from './fog_1.png';
+import fog_2 from './fog_2.png';
+import fog_3 from './fog_3.png';
+import fog_4 from './fog_4.png';
+import fog_5 from './fog_5.png';
+import fog_6 from './fog_6.png';
+import fog_7 from './fog_7.png';
+import mountain_1 from './mountain_1.png';
+import mountain_2 from './mountain_2.png';
+import mountain_3 from './mountain_3.png';
+import mountain_4 from './mountain_4.png';
+import mountain_5 from './mountain_5.png';
+import mountain_6 from './mountain_6.png';
+import mountain_7 from './mountain_7.png';
+import mountain_8 from './mountain_8.png';
+import mountain_9 from './mountain_9.png';
+import mountain_10 from './mountain_10.png';
+import sun_rays from './sun_rays.png';
+export {
+    background,
+    black_shadow,
+    fog_1,
+    fog_2,
+    fog_3,
+    fog_4,
+    fog_5,
+    fog_6,
+    fog_7,
+    mountain_1,
+    mountain_2,
+    mountain_3,
+    mountain_4,
+    mountain_5,
+    mountain_6,
+    mountain_7,
+    mountain_8,
+    mountain_9,
+    mountain_10,
+    sun_rays
+}
