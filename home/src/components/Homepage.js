7ba87c214157bf54e9946956bab0e973d476e94e
Cqctxs <seanzhao2007@gmail.com>
2023-10-24 11:33:22 -0400

    Cool effect :D

--- home/src/components/Homepage.js
+++ home/src/components/Homepage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import Image from './Image';
 import {
   background,
   black_shadow,
@@ -38,6 +37,7 @@ function Homepage() {
     }
 
     setElements(document.querySelectorAll(".parallax"));
+    console.log(document.querySelectorAll(".parallax"));
     window.addEventListener("mousemove", mouseMove);
 
     return () => {
@@ -47,36 +47,38 @@ function Homepage() {
 
   useEffect(() => {
     elements.forEach(el => {
-      el.style.transform = `translate(calc(-50% + ${mousePosition.x}px), calc(-50% + ${mousePosition.y}px))`;
+      const x = parseFloat(el.getAttribute('data-x'));
+      const y = parseFloat(el.getAttribute('data-y'));
+      el.style.transform = `translate(calc(-50% + ${(mousePosition.x * x)}px), calc(-50% + ${mousePosition.y * y}px))`;
     })
   }, [mousePosition, elements])
   return (
     <main>
       <div className="vingette" />
-      <Image image={background} className="parallax bg-img" />
-      <Image image={fog_7} className="parallax fog-7" />
-      <Image image={mountain_10} className="parallax mountain-10" />
-      <Image image={fog_6} className="parallax fog-6" />
-      <Image image={mountain_9} className="parallax mountain-9" />
-      <Image image={mountain_8} className="parallax mountain-8" />
-      <Image image={fog_5} className="parallax fog-5" />
-      <Image image={mountain_7} className="parallax mountain-7" />
-      <div className="parallax text">
+      <img src={background} alt="" className="parallax bg-img" data-x={0.1} data-y={0.1} />
+      <img src={fog_7} alt="" className="parallax fog-7" data-x={0.2} data-y={0.2} />
+      <img src={mountain_10} alt="" className="parallax mountain-10" data-x={0.3} data-y={0.3} />
+      <img src={fog_6} alt="" className="parallax fog-6" data-x={0.4} data-y={0.4} />
+      <img src={mountain_9} alt="" className="parallax mountain-9" data-x={0.5} data-y={0.5} />
+      <img src={mountain_8} alt="" className="parallax mountain-8" data-x={0.6} data-y={0.6} />
+      <img src={fog_5} alt="" className="parallax fog-5" data-x={0.7} data-y={0.7} />
+      <img src={mountain_7} alt="" className="parallax mountain-7" data-x={0.8} data-y={0.8} />
+      <div className="parallax text" data-x={0.8} data-y={0.8}>
         <h2>Hello</h2>
         <h1>World</h1>
       </div>
-      <Image image={mountain_6} className="parallax mountain-6" />
-      <Image image={fog_4} className="parallax fog-4" />
-      <Image image={mountain_5} className="parallax mountain-5" />
-      <Image image={fog_3} className="parallax fog-3" />
-      <Image image={mountain_4} className="parallax mountain-4" />
-      <Image image={mountain_3} className="parallax mountain-3" />
-      <Image image={fog_2} className="parallax fog-2" />
-      <Image image={mountain_2} className="parallax mountain-2" />
-      <Image image={mountain_1} className="parallax mountain-1" />
-      <Image image={sun_rays} className="sun-rays" />
-      <Image image={black_shadow} className="black-shadow" />
-      <Image image={fog_1} className="parallax fog-1" />
+      <img src={mountain_6} alt="" className="parallax mountain-6" data-x={0.9} data-y={0.9} />
+      <img src={fog_4} alt="" className="parallax fog-4" data-x={1} data-y={1} />
+      <img src={mountain_5} alt="" className="parallax mountain-5" data-x={1.1} data-y={1.1} />
+      <img src={fog_3} alt="" className="parallax fog-3" data-x={1.2} data-y={1.2} />
+      <img src={mountain_4} alt="" className="parallax mountain-4" data-x={1.3} data-y={1.3} />
+      <img src={mountain_3} alt="" className="parallax mountain-3" data-x={1.4} data-y={1.4} />
+      <img src={fog_2} alt="" className="parallax fog-2" data-x={1.5} data-y={1.5} />
+      <img src={mountain_2} alt="" className="parallax mountain-2" data-x={1.6} data-y={1.6} />
+      <img src={mountain_1} alt="" className="parallax mountain-1" data-x={1.7} data-y={1.7} />
+      <img src={sun_rays} alt="" className="sun-rays" data-x={1.8} data-y={1.8} />
+      <img src={black_shadow} alt="" className="black-shadow" data-x={1.9} data-y={1.9} />
+      <img src={fog_1} alt="" className="parallax fog-1" data-x={2} data-y={2} />
     </main>
   )
 }
