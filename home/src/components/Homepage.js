a0e863d7dfa659bdfa73c888dd37b95b38667650
Cqctxs <seanzhao2007@gmail.com>
2023-10-24 21:46:56 -0400

    Updated values to look better

--- home/src/components/Homepage.js
+++ home/src/components/Homepage.js
@@ -55,30 +55,30 @@ function Homepage() {
   return (
     <main>
       <div className="vingette" />
-      <img src={background} alt="" className="parallax bg-img" data-x={0.05} data-y={0.05} />
-      <img src={fog_7} alt="" className="parallax fog-7" data-x={0.1} data-y={0.1} />
-      <img src={mountain_10} alt="" className="parallax mountain-10" data-x={0.15} data-y={0.15} />
-      <img src={fog_6} alt="" className="parallax fog-6" data-x={0.2} data-y={0.2} />
-      <img src={mountain_9} alt="" className="parallax mountain-9" data-x={0.25} data-y={0.25} />
-      <img src={mountain_8} alt="" className="parallax mountain-8" data-x={0.3} data-y={0.3} />
-      <img src={fog_5} alt="" className="parallax fog-5" data-x={0.35} data-y={0.35} />
-      <img src={mountain_7} alt="" className="parallax mountain-7" data-x={0.4} data-y={0.4} />
-      <div className="parallax text" data-x={0.45} data-y={0.45}>
+      <img src={background} alt="" className="parallax bg-img" data-x={0.3} data-y={0.38} />
+      <img src={fog_7} alt="" className="parallax fog-7" data-x={0.27} data-y={0.32} />
+      <img src={mountain_10} alt="" className="parallax mountain-10" data-x={0.195} data-y={0.305} />
+      <img src={fog_6} alt="" className="parallax fog-6" data-x={0.25} data-y={0.28} />
+      <img src={mountain_9} alt="" className="parallax mountain-9" data-x={0.125} data-y={0.155} />
+      <img src={mountain_8} alt="" className="parallax mountain-8" data-x={0.1} data-y={0.11} />
+      <img src={fog_5} alt="" className="parallax fog-5" data-x={0.16} data-y={0.105} />
+      <img src={mountain_7} alt="" className="parallax mountain-7" data-x={0.1} data-y={0.1} />
+      <div className="parallax text" data-x={0.07} data-y={0.07}>
         <h2>Hello</h2>
         <h1>World</h1>
       </div>
-      <img src={mountain_6} alt="" className="parallax mountain-6" data-x={0.5} data-y={0.5} />
-      <img src={fog_4} alt="" className="parallax fog-4" data-x={0.55} data-y={0.55} />
-      <img src={mountain_5} alt="" className="parallax mountain-5" data-x={0.6} data-y={0.6} />
-      <img src={fog_3} alt="" className="parallax fog-3" data-x={0.65} data-y={0.65} />
-      <img src={mountain_4} alt="" className="parallax mountain-4" data-x={0.7} data-y={0.7} />
-      <img src={mountain_3} alt="" className="parallax mountain-3" data-x={0.75} data-y={0.75} />
-      <img src={fog_2} alt="" className="parallax fog-2" data-x={0.8} data-y={0.8} />
-      <img src={mountain_2} alt="" className="parallax mountain-2" data-x={0.85} data-y={0.85} />
-      <img src={mountain_1} alt="" className="parallax mountain-1" data-x={0.9} data-y={0.9} />
+      <img src={mountain_6} alt="" className="parallax mountain-6" data-x={0.065} data-y={0.05} />
+      <img src={fog_4} alt="" className="parallax fog-4" data-x={0.135} data-y={0.05} />
+      <img src={mountain_5} alt="" className="parallax mountain-5" data-x={0.06} data-y={0.03} />
+      <img src={fog_3} alt="" className="parallax fog-3" data-x={0.11} data-y={0.018} />
+      <img src={mountain_4} alt="" className="parallax mountain-4" data-x={0.059} data-y={0.024} />
+      <img src={mountain_3} alt="" className="parallax mountain-3" data-x={0.04} data-y={0.018} />
+      <img src={fog_2} alt="" className="parallax fog-2" data-x={0.15} data-y={0.0115} />
+      <img src={mountain_2} alt="" className="parallax mountain-2" data-x={0.0235} data-y={0.013} />
+      <img src={mountain_1} alt="" className="parallax mountain-1" data-x={0.027} data-y={0.018} />
       <img src={sun_rays} alt="" className="sun-rays" />
       <img src={black_shadow} alt="" className="black-shadow" />
-      <img src={fog_1} alt="" className="parallax fog-1" data-x={0.15} data-y={0.15} />
+      <img src={fog_1} alt="" className="parallax fog-1" data-x={0.12} data-y={0.01} />
     </main>
   )
 }
